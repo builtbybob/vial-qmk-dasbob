ed8f56f0fe8c658a7e337a26d271464b6208fbdf
Bob <robertbarton86@gmail.com>
2026-02-08 21:59:48 -0500

    Add custom layout for dasbob keyboard, with support for combos on mod-tap keys.

---
+++ keyboards/dasbob/keymaps/bob/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x33, 0x7A, 0x81, 0x10, 0xBF, 0xCB, 0x21, 0x73}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+#define COMBO_ALLOW_ACTION_KEYS
